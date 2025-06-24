00e018b31279703dbff94dce630deaccae98ab69
Ne Chelovek <zimooov@gmail.com>
2025-06-25 01:52:21 +0300

    added some of the utilities

---
+++ migrations/20250511170311_role_endpoint..sql
@@ -0,0 +1,27 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE role_endpoints (
+    role TEXT NOT NULL,
+    endpoint TEXT NOT NULL,
+    PRIMARY KEY (role, endpoint)
+);
+
+INSERT INTO role_endpoints (role, endpoint) VALUES
+    ('admin', '/chat_v1.Chat/Create'),
+    ('admin', '/chat_v1.Chat/Get'),
+    ('admin', '/chat_v1.Chat/Delete'),
+    ('admin', '/chat_v1.Chat/SendMessage'),
+    ('admin', '/chat_v1.Chat/Connect'),
+    ('user', '/chat_v1.Chat/Get'),
+    ('user', '/chat_v1.Chat/Create'),
+    ('user', '/chat_v1.Chat/SendMessage'),
+    ('user', '/chat_v1.Chat/Delete'),
+    ('user', '/chat_v1.Chat/Connect');
+
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS role_endpoints;
+-- +goose StatementEnd
